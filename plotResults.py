6ed2d9b67a5c407e6e20880a8af4d1c7b1835c35
Christoph Tack <prog.send@gmail.com>
2023-02-05 17:44:52 +0100

    Update plot functions

--- plotResults.py
+++ plotResults.py
@@ -5,23 +5,42 @@ import matplotlib.pyplot as plt
 import numpy as np
 # Formulas from [Owen Duffy](https://owenduffy.net/calc/s11s21toloss.htm)
 
-def plot_s_parameters(filepath):
+def plot_s_parameters(nw):
     '''
     Plot S-parameters
     filepath = file to Touchstone S2P file.
     '''
-    nw = rf.Network(filepath)
     fig, ax = plt.subplots()
     nw.plot_s_db(m=0, n=0, ax=ax, color='r')
     nw.plot_s_db(m=1, n=0, ax=ax, color='b')
     ax.grid()
     ax.set_ylabel('S-Parameters [dB]')
 
-def calc_load_loss_dB(filepath):
+def plot_vswr(nw):
+    fig, ax = plt.subplots()
+    nw.plot_s_vswr(m=0,n=0,color='r', ax=ax,logx=False)
+    ax.grid()
+
+def plot_impedance(nw):
+    fig, (ax1, ax2) = plt.subplots(1,2)
+    nw.plot_z_re(m=0, n=0, ax=ax1, color='g')
+    nw.plot_z_im(m=0, n=0, ax=ax2, color='b')
+    ax1.grid()
+    ax2.grid()
+
+def plot_inductance(nw, figtitle):
+    plt.subplots()
+    L = nw.z.imag[:,0,0] / (2 * math.pi * nw.f)
+
+    plt.title(figtitle)
+    plt.ylabel('Inductance [H]')
+    plt.xlabel('Frequency [Hz]')
+    plt.plot(nw.f, L)
+
+def calc_load_loss_dB(nw):
     '''
     Calculate loss of connecting 2400Ω resistor in series between port 1 and port 2 using as short a connection as possible, no SMA-cable used.
     '''
-    nw = rf.Network(filepath)
     s11_mag = nw.s11.s_mag.squeeze()
     s21_mag = nw.s21.s_mag.squeeze()
     # https://owenduffy.net/blog/?p=20312
@@ -34,23 +53,22 @@ def calc_load_loss_dB(filepath):
     #plt.ylabel('Loss')
     #plt.show()
 
-def plot_transformer_loss(filepath_transformer, filepath_2400_cal, graph_title):
+def plot_transformer_loss(nw, nw_cal, graph_title):
     '''
     Plot transformer loss
-    filepath_transformer = file to Touchstone S2P file.
-    filepath_2400_cal = Touchstone S2P file of calibration measurement of 2400 ohm between port 1 and port 2
+    nw = network from Touchstone S2P file.
+    nw_cal = network from Touchstone S2P file of calibration measurement of 2400 ohm between port 1 and port 2
     graph_title = title of the graph
     
     return loss_db
     '''
     fig, ax = plt.subplots()
-    nw = rf.Network(filepath_transformer)
     s11_mag = nw.s11.s_mag.squeeze()
     s21_mag = nw.s21.s_mag.squeeze()
     #insertion loss [dB] = -20*log10(|s21|)
     insertion_loss_db = -20*np.log10(s21_mag)
     # Correct for the loss of the 2400Ω series resistor
-    attenuator_loss_db = calc_load_loss_dB(filepath_2400_cal)
+    attenuator_loss_db = calc_load_loss_dB(nw_cal)
     insertion_loss_db = insertion_loss_db - attenuator_loss_db 
     #return loss [dB] = -20*log10(|s11|)
     #mismatch loss [dB] = -10*log10(1-|s11|²)
